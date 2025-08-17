712b6bbe20b94ac4526780356ef52f5a20fcb0b4
Ketan Gathibandhe <ketangathibandhe04@gmail.com>
2025-08-17 13:59:40 +0530

    currentSkills api

--- routes/profile.js
+++ routes/profile.js
@@ -1,16 +1,26 @@
-const express = require('express')
+const express = require("express");
 const profileRouter = express.Router();
-const{userAuth} = require('../middleware/auth')
-const User = require('../models/user')
+const { userAuth } = require("../middleware/auth");
+const User = require("../models/user");
 
 profileRouter.post("/set-role", userAuth, async (req, res) => {
-try {
-      const { targetRole } = req.body;
-  await User.findByIdAndUpdate(req.user._id, { targetRole });
-  res.json({ message: "Target role updated!" });
-} catch (error) {
-    res.send("something went wrong.."+error.message)
-}
+  try {
+    const { targetRole } = req.body;
+    await User.findByIdAndUpdate(req.user._id, { targetRole });
+    res.json({ message: "Target role updated!" });
+  } catch (error) {
+    res.send("something went wrong.." + error.message);
+  }
 });
 
-module.exports= profileRouter;
+profileRouter.post("/add-skills", userAuth, async (req, res) => {
+  try {
+    const { currentSkills } = req.body; // ["HTML","CSS","JS"]
+    await User.findByIdAndUpdate(req.user._id, { currentSkills });
+    res.json({ message: "Skills updated!" });
+  } catch (error) {
+    res.send("something went wrong.." + error.message);
+  }
+});
+
+module.exports = profileRouter;
